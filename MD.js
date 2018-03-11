ab791b66769a218358e5e59e495296479565296c
bivi325 <bivi325@gmail.com>
2018-03-11 21:43:34 +0200

    Added methods
    Made main methods. Next stop is writing to a file, animation, pereodic property.

--- MD.js
+++ MD.js
@@ -19,16 +19,17 @@ const charge_s = 2.0;
 const O_coordinates = starting_data.Oxygen;
 const Zn_coordinates = starting_data.Zunk;
 const N = starting_data.Number;
+const t = 1e-15;
 
 class Molecule{
-	constructor(x = 0,y = 0,z = 0){
+	constructor(x = 0, y = 0, z = 0){
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.forceX = 0;
 		this.forceY = 0;
 		this.forceZ = 0;
-		this.accelerationX = 0;
+		this.accelerationX = 0.0;
 		this.accelerationY = 0;
 		this.accelerationZ = 0;
 		this.speedX = 0;
@@ -53,8 +54,10 @@ const Zunk = new Array(N);
 for(let i = 0; i < Zunk.length; i++){
 	Zunk[i] = new Molecule(Zn_coordinates.x[i],Zn_coordinates.y[i],Zn_coordinates.z[i]);
 }
+
 function countForce(Oxygen, Zunk) {
-    let dx = dy = dz = dr = 0;
+		let dx = dy = dz = dr = 0;
+		// Counting force between O-O molecules
     for (let i = 0; i < N; i++) {
         for (let j = i + 1; j < N; j++) {
         	dx = Oxygen[i].x - Oxygen[j].x;
@@ -68,9 +71,8 @@ function countForce(Oxygen, Zunk) {
         	Oxygen[i].forceZ = Oxygen[i].forceZ + (dz/dr) * force_O_O(dr);
         	Oxygen[j].forceZ = Oxygen[j].forceZ - (dz/dr) * force_O_O(dr);
         }
-    }
-    dx = dy = dz = dr = 0;
-
+		}
+		// Counting force between Zn-O molecules
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < N; j++) {
         	dx = Oxygen[i].x - Zunk[j].x;
@@ -86,16 +88,72 @@ function countForce(Oxygen, Zunk) {
         }
     }
 }
+function countAcceleration(Oxygen, Zunk) {
+	for (let i = 0; i < N; i++) {
+			Oxygen[i].accelerationX = Oxygen[i].forceX / m_O;
+			Oxygen[i].accelerationY = Oxygen[i].forceY / m_O;
+			Oxygen[i].accelerationZ = Oxygen[i].forceZ / m_O;
+			Zunk[i].accelerationX = Zunk[i].forceX / m_Zn;
+			Zunk[i].accelerationY = Zunk[i].forceY / m_Zn;
+			Zunk[i].accelerationZ = Zunk[i].forceZ / m_Zn;					
+	}
+}
+function countSpeed(Oxygen, Zunk) {
+	for (let i = 0; i < N; i++) {
+			Oxygen[i].speedX = Oxygen[i].speedX + Oxygen[i].accelerationX * t;
+			Oxygen[i].speedY = Oxygen[i].speedY + Oxygen[i].accelerationY * t;
+			Oxygen[i].speedZ = Oxygen[i].speedY + Oxygen[i].accelerationY * t;
+			Zunk[i].speedX = Zunk[i].speedX + Zunk[i].accelerationX * t;
+			Zunk[i].speedY = Zunk[i].speedY + Zunk[i].accelerationY * t;
+			Zunk[i].speedZ = Zunk[i].speedY + Zunk[i].accelerationY * t;				
+	}
+}
+function countCoordinates(Oxygen, Zunk) {
+	for (let i = 0; i < N; i++) {
+			Oxygen[i].x = Oxygen[i].x + Oxygen[i].speedX * t;
+			Oxygen[i].y = Oxygen[i].y + Oxygen[i].speedY * t;
+			Oxygen[i].z = Oxygen[i].z + Oxygen[i].speedY * t;
+			Zunk[i].x = Zunk[i].x + Zunk[i].speedX * t;
+			Zunk[i].y = Zunk[i].y + Zunk[i].speedY * t;
+			Zunk[i].z = Zunk[i].z + Zunk[i].speedY * t;				
+	}
+}
 
 function force_O_O(r) {
   force = k * (charge_f * charge_f) / (r * r) + c * (A_O_O / p_O_O) * Math.exp(-Math.abs(r) / p_O_O) - c * 6 * C_O_O / Math.pow(Math.abs(r), 7.0);
   return force;
 }
-
 function force_Zn_O(r) {
   force = k * (charge_s * charge_f) / (r * r) + c * (A_Zn_O / p_Zn_O) * Math.exp(-Math.abs(r) / p_Zn_O);
   return force;
 }
-countForce(Oxygen, Zunk);
+const startO = JSON.parse(JSON.stringify(Oxygen));
 
-console.log(Zunk);
+const interval = setInterval(() =>{	
+	countForce(Oxygen, Zunk);
+	countAcceleration(Oxygen, Zunk);
+	countSpeed(Oxygen, Zunk);
+	countCoordinates(Oxygen, Zunk);
+	end = new Date(milliseconds);
+	console.log(end - start);	
+}, 4);
+
+setTimeout(() =>{
+	clearInterval(interval);
+	print();
+}, 200000);
+
+// let start,end;
+// start = new Date();
+// countForce(Oxygen, Zunk);
+// countAcceleration(Oxygen, Zunk);
+// countSpeed(Oxygen, Zunk);
+// countCoordinates(Oxygen, Zunk);
+// end = new Date();
+// console.log(end - start);	
+
+function print(){
+	for(let i = 0; i < N; i++){
+		console.log(startO[i].x - Oxygen[i].x);	
+	}
+}
