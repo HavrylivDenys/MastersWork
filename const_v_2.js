3649c98c7dbb642b54ab72e775754816f2ddaad8
bivi325 <bivi325@gmail.com>
2018-04-13 00:11:57 +0300

    try

---
+++ const_v_2.js
@@ -0,0 +1,33 @@
+module.exports = {
+  constants: {
+    c_force: "1", // 
+    m: "1.660538782E-27", // kg
+    el: "1.60217653E-19",
+    k: "1", // 
+
+    charge_Zn: "2.0",
+    charge_O: "-2.0",
+    m_Zn: "60910.39E6", // kg
+    m_O: "14903.34E6", // kg
+
+    A_O_O: "9547.96",
+    A_Zn_O: "529.7",
+    A_Zn_Zn: "0.0",
+    p_O_O: "0.21916",
+    p_Zn_O: "0.3581",
+    p_Zn_Zn: "0.0",
+    C_O_O: "32.0",
+    C_Zn_O: "0.0",
+    C_Zn_Zn: "0.0",
+
+    A_O_O_Nyberg: "22764.0",
+    A_Zn_O_Nyberg: "499.6",
+    A_Zn_Zn_Nyberg: "0.0",
+    p_O_O_Nyberg: "0.149",
+    p_Zn_O_Nyberg: "0.3595",
+    p_Zn_Zn_Nyberg: "0.0",
+    C_O_O_Nyberg: "27.88",
+    C_Zn_O_Nyberg: "0.0",
+    C_Zn_Zn_Nyberg: "0.0"
+  }
+};
